24032f33550ba0948b5c9925a2fea039c0b6bad3
Anderson Zavelinski <zavelinski20@gmail.com>
2023-09-12 21:01:21 -0300

    Codelab-Angular-LocacaoDeImoveis
    Codelab o qual eu fiz algumas alterações do projeto modelo, trazendo uma versão traduzida e condizente com a realidade Brasileira

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fairhouse';
+  housingLocationList: HousingLocation[] = [
+    {
+      name: "Moradia com 4 quartos, segura e confortável",
+      city: "Laranjeiras do Sul",
+      state: "Paraná",
+      photo: "../assets/housing-1.jpg",
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "A113 Casa com 3 quartos, bem localizada",
+      city: "Dois Vizinhos",
+      state: "Paraná",
+      photo: "../assets/housing-2.jpg",
+      availableUnits: 3,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      name: "Camas aquecidas Suporte Habitacional",
+      city: "Joinville",
+      state: "SC",
+      photo: "../assets/housing-3.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    }
+  ];
+
+  selectedLocation: HousingLocation | undefined;
+
+  updateSelectedLocation(location: HousingLocation) { 
+    this.selectedLocation = location;
+  } searchHousingLocations() {}
+
+
 }
